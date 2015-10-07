81e170026d02f8de1e6140173330a8ec606319ac
Darpan Sharma <darpan_sharma@intuit.com>
2015-10-07 16:33:33 -0500

    putting back @component

--- src/main/java/com/first/hr/repository/EmployeeRepository.java
+++ src/main/java/com/first/hr/repository/EmployeeRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Component;
 
 import com.first.hr.domain.Employee;
 @RepositoryRestResource(collectionResourceRel = "employee", path = "employee")
+@Component
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
 	List<Employee> findByLastNameContaining(@Param("name") String name);
 
